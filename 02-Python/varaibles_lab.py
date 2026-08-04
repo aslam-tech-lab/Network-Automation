b2e8e0eb33fae1dea812a0f8c9a127ada690ddd4
Aslam <aslam.mohammad87@gmail.com>
2026-08-04 13:28:35 +0530

    adding python files

---
+++ 02-Python/varaibles_lab.py
@@ -0,0 +1,20 @@
+print('=' * 60)
+print(' Variables Lab ')
+print('=' * 60)
+
+device_hostname = " Leaf1"
+device_vendor = "Cisco"
+device_platform = "Nexus 9000"
+device_os_version = "NXOS-10.2"
+device_management_ip = '192.168.1.2'
+vlan_id = "200"
+bgp_ASN_ID = "65535"
+
+print("Hostname = ", device_hostname)
+print("Vendor = ", device_vendor)
+print("Platform = ", device_platform)
+print("Version = ", device_os_version)
+print("IP address = ", device_management_ip)
+print("Vlan number = ", vlan_id)
+print("ASN = ", bgp_ASN_ID)
+
